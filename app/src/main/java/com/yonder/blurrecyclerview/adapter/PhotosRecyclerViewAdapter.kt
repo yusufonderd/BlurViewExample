86d102179796945099685f441404a09e5ee14dcf
YusufMac <yusuf.onder@appcent.mobi>
2021-03-26 21:36:16 +0300

    added comment line for little details

--- app/src/main/java/com/yonder/blurrecyclerview/adapter/PhotosRecyclerViewAdapter.kt
+++ app/src/main/java/com/yonder/blurrecyclerview/adapter/PhotosRecyclerViewAdapter.kt
@@ -19,9 +19,15 @@ class PhotosRecyclerViewAdapter(private val titles: List<String>) :
             .inflate(R.layout.item_photo, parent, false)
         return PhotoItemViewHolder(view).also { viewHolder ->
             viewHolder.blurView.apply {
+                // use outlineProvider and clipToOutline if you're using background drawable inside BlurView
+                // rounded rectangle background drawable of BlurView: @drawable/bg_blur_item_photo
+                // you don't have to use background drawable
+                // check documentation from https://github.com/Dimezis/BlurView
                 outlineProvider = ViewOutlineProvider.BACKGROUND
                 clipToOutline = true
 
+                // setup blur view
+                // if you don't setup it doesn't work
                 setupWith(parent)
                     .setBlurAlgorithm(RenderScriptBlur(viewHolder.clRoot.context))
                     .setBlurRadius(BLUR_RADIUS)
